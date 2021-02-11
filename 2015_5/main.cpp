12bc028e06407e76d858cdf8d8d4ff889cbd5abe
harshsr <69389429+harshsr@users.noreply.github.com>
2021-02-11 10:57:39 +0530

    Function logic remaining

--- 2015_5/main.cpp
+++ 2015_5/main.cpp
@@ -3,7 +3,76 @@
 
 using namespace std;
 
+struct Vowels
+{
+	char a, e, i, o, u;
+}vowels;
+//setting vowels
+vowels.a = 'a';
+vowels.e = 'e';
+vowels.i = 'i';
+vowels.o = 'o';
+vowels.u = 'u';
+
+struct NaughtySubstrings {
+	string ab, cd, pq, xy;
+}naughtySubstrings;
+//setting naughty substrings
+naughtySubstrings.ab = "ab";
+naughtySubstrings.cd = "cd";
+naughtySubstrings.pq = "pq";
+naughtySubstrings.xy = "xy";
+
+
+
+bool VowelReq(string s)
+{
+
+}
+
+bool LetterRepetitionReq(string s)
+{
+
+}
+
+bool NaughtySubstrReq(string s)
+{
+
+}
+
+
+bool StringChecker(string s)
+{
+	if (VowelReq(s) && LetterRepetitionReq(s) && NaughtySubstrReq(s))
+	{
+		return true;
+	}
+
+	return false;
+}
+
 int main()
 {
+
+
+
 	ifstream inputList;
+	inputList.open("list.txt");
+	if (!inputList)
+	{
+		std::cerr << "Unable to open file datafile.txt";
+		exit(1);
+	}
+
+	string s;
+	int numberOfNiceStrings = 0;
+	while (getline(inputList, s))
+	{
+		if (StringChecker(s))
+		{
+			numberOfNiceStrings++;
+		}
+	}
+
+	cout << "Number of nice strings is: " << numberOfNiceStrings << "\n";
 }
